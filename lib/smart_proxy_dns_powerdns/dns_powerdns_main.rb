1ba6977d9638931846a55babe8799bf531a89d41
Ewoud Kohl van Wijngaarden <ekohlvanwijngaarden@antagonist.nl>
2015-07-11 12:31:59 +0200

    Rewrite to mysql2 and many things
    Closes GH-2

--- lib/smart_proxy_dns_powerdns/dns_powerdns_main.rb
+++ lib/smart_proxy_dns_powerdns/dns_powerdns_main.rb
@@ -1,36 +1,45 @@
 require 'dns/dns'
-require 'mysql'
+require 'mysql2'
 
 module Proxy::Dns::Powerdns
   class Record < ::Proxy::Dns::Record
     include Proxy::Log
     include Proxy::Util
 
-    attr_reader :powerdns_mysql_hostname
-    attr_reader :powerdns_mysql_username
-    attr_reader :powerdns_mysql_password
-    attr_reader :powerdns_mysql_database
+    attr_reader :powerdns_mysql_hostname, :powerdns_mysql_username, :powerdns_mysql_password, :powerdns_mysql_database
+
+    def self.record(attrs = {})
+      new(attrs.merge(
+        :powerdns_mysql_hostname => ::Proxy::Dns::Powerdns::Plugin.settings.powerdns_mysql_hostname,
+        :powerdns_mysql_username => ::Proxy::Dns::Powerdns::Plugin.settings.powerdns_mysql_username,
+        :powerdns_mysql_password => ::Proxy::Dns::Powerdns::Plugin.settings.powerdns_mysql_password,
+        :powerdns_mysql_database => ::Proxy::Dns::Powerdns::Plugin.settings.powerdns_mysql_database,
+      ))
+    end
 
     def initialize options = {}
       raise "dns_powerdns provider needs 'powerdns_mysql_hostname' option" unless options[:powerdns_mysql_hostname]
       raise "dns_powerdns provider needs 'powerdns_mysql_username' option" unless options[:powerdns_mysql_username]
       raise "dns_powerdns provider needs 'powerdns_mysql_password' option" unless options[:powerdns_mysql_password]
       raise "dns_powerdns provider needs 'powerdns_mysql_database' option" unless options[:powerdns_mysql_database]
-      @mysql_connection = Mysql.new(options[:powerdns_mysql_hostname], options[:powerdns_mysql_username], options[:powerdns_mysql_password], options[:powerdns_mysql_database])
+      @mysql_connection = Mysql2::Client.new(
+        :host => options[:powerdns_mysql_hostname],
+        :username => options[:powerdns_mysql_username],
+        :password => options[:powerdns_mysql_password],
+        :database => options[:powerdns_mysql_database],
+      )
       super(options)
     end
 
     def create
-      unless domain_id
-        raise Proxy::DNS::Error, "Unable to determine zone. Zone must exist in PowerDNS."
-      end
+      raise Proxy::Dns::Error, "Unable to determine zone. Zone must exist in PowerDNS." unless domain_id
 
       case @type
         when "A"
           if ip = dns_find(@fqdn)
-            raise Proxy::DNS::Collision, "#{@fqdn} is already in use by #{ip}"
+            raise Proxy::Dns::Collision, "#{@fqdn} is already in use by #{ip}"
           else
-            @mysql_connection.query("INSERT INTO records (records.domain_id,records.name,records.ttl,records.content,records.type) VALUES (#{domain_id}, '#{@fqdn}', #{@ttl}, '#{@value}', '#{@type}');")
+            create_record(domain_id, @fqdn, @ttl, @value, @type)
           end
 
           true
@@ -38,9 +47,9 @@ module Proxy::Dns::Powerdns
           ip = IPAddr.new(@value)
           ptrname = ip.reverse
           if name = dns_find(ptrname)
-            raise Proxy::DNS::Collision, "#{@value} is already used by #{name}"
+            raise Proxy::Dns::Collision, "#{@value} is already used by #{name}"
           else
-            @mysql_connection.query("INSERT INTO records (records.domain_id,records.name,records.ttl,records.content,records.type) VALUES (#{domain_id}, '#{ptrname}', #{@ttl}, '#{@fqdn}', 'PTR');")
+            create_record(domain_id, ptrname, @ttl, @fqdn, @type)
           end
 
           true
@@ -50,13 +59,13 @@ module Proxy::Dns::Powerdns
     def remove
       case @type
         when "A"
-          @mysql_connection.query("DELETE FROM records WHERE name='#{@fqdn}' AND type='#{@type}'")
+          delete_record(@fqdn, @type)
 
           true
         when "PTR"
           ip = IPAddr.new(@value)
           ptrname = ip.reverse
-          @mysql_connection.query("DELETE FROM records WHERE name='#{ptrname}' AND type='#{@type}'")
+          delete_record(ptrname, @type)
 
           true
       end
@@ -66,43 +75,52 @@ module Proxy::Dns::Powerdns
     def domain_id
       case @type
       when "A"
-        host_list = @fqdn.split(/\./);
-        search_depth = 1
+        name = @fqdn
       when "PTR"
-        host_list = @value.split(/\./).reverse;
-        search_depth = 0
+        ip = IPAddr.new(@value)
+        name = ip.reverse
       end
 
       id = nil
 
-      while id == nil && host_list.length != search_depth && host_list != nil
-        domain = host_list * "."
-        if @type == "PTR"
-          domain.concat(".in-addr.arpa")
-        end
-        res = @mysql_connection.query("SELECT id FROM domains WHERE name = '#{domain}' LIMIT 1;")
-        if res.num_rows() != 0
-          id = res.fetch_row
-        end
-        res.free
-        host_list.delete_at(0)
+      name = @mysql_connection.escape(name)
+      @mysql_connection.query("SELECT LENGTH(name) domain_length, id FROM domains WHERE '#{name}' LIKE CONCAT('%%.', name) ORDER BY domain_length DESC LIMIT 1").each do |row|
+        id = row["id"]
       end
+
       id
     end
 
     private
     def dns_find key
       value = nil
-      res = @mysql_connection.query("SELECT content FROM records WHERE name = '#{key}' LIMIT 1;")
-      if res.num_rows() != 0
-        value = res.fetch_row
+      key = @mysql_connection.escape(key)
+      @mysql_connection.query("SELECT content FROM records WHERE domain_id=#{domain_id} AND name = '#{key}' LIMIT 1").each do |row|
+        value = row["content"]
       end
-      res.free
       if value != nil
         value
       else
         false
       end
+    end
+
+    private
+    def create_record domain_id, name, ttl, content, type
+      name = @mysql_connection.escape(name)
+      content = @mysql_connection.escape(content)
+      ttl = @mysql_connection.escape(ttl)
+      type = @mysql_connection.escape(type)
+      @mysql_connection.query("INSERT INTO records (domain_id, name, ttl, content, type) VALUES (#{domain_id}, '#{name}', #{ttl}, '#{content}', '#{type}')")
+      # TODO: run rectify-zone
+    end
+
+    private
+    def delete_record name, type
+      name = @mysql_connection.escape(name)
+      type = @mysql_connection.escape(type)
+      @mysql_connection.query("DELETE FROM records WHERE name='#{name}' AND type='#{type}'")
+      # TODO: run rectify-zone
     end
   end
 end
--- smart_proxy_dns_powerdns.gemspec
+++ smart_proxy_dns_powerdns.gemspec
@@ -19,5 +19,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency('rake')
   s.add_development_dependency('mocha')
 
-  s.add_dependency('mysql')
+  s.add_dependency('mysql2')
 end
